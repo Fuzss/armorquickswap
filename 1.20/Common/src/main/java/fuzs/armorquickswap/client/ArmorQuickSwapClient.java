b3473d18f70be4e27ff137fb8e813be59b83b372
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-11-08 00:17:48 +0100

    move unrelated content to separate projects

--- 1.20/Common/src/main/java/fuzs/armorquickswap/client/ArmorQuickSwapClient.java
+++ 1.20/Common/src/main/java/fuzs/armorquickswap/client/ArmorQuickSwapClient.java
@@ -1,25 +1,16 @@
 package fuzs.armorquickswap.client;
 
 import fuzs.armorquickswap.ArmorQuickSwap;
-import fuzs.armorquickswap.client.handler.*;
-import fuzs.armorquickswap.client.init.BloodParticle;
-import fuzs.armorquickswap.client.init.ClientModRegistry;
+import fuzs.armorquickswap.client.handler.ClickableAdvancementsHandler;
+import fuzs.armorquickswap.client.handler.InventoryArmorClickHandler;
+import fuzs.armorquickswap.client.handler.LocalArmorStandGearHandler;
 import fuzs.puzzlesaccessapi.api.client.data.v2.BlockModelBuilder;
 import fuzs.puzzlesaccessapi.api.client.data.v2.ItemModelBuilder;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
-import fuzs.puzzleslib.api.client.core.v1.context.KeyMappingsContext;
-import fuzs.puzzleslib.api.client.core.v1.context.ParticleProvidersContext;
 import fuzs.puzzleslib.api.client.data.v2.AbstractModelProvider;
 import fuzs.puzzleslib.api.client.event.v1.*;
-import fuzs.puzzleslib.api.client.screen.v2.KeyMappingActivationHelper;
-import fuzs.puzzleslib.api.core.v1.ContentRegistrationFlags;
 import fuzs.puzzleslib.api.core.v1.context.PackRepositorySourcesContext;
-import fuzs.puzzleslib.api.event.v1.LoadCompleteCallback;
 import fuzs.puzzleslib.api.event.v1.core.EventPhase;
-import fuzs.puzzleslib.api.event.v1.entity.living.LivingAttackCallback;
-import fuzs.puzzleslib.api.event.v1.entity.living.LivingDeathCallback;
-import fuzs.puzzleslib.api.event.v1.entity.living.LivingEvents;
-import fuzs.puzzleslib.api.event.v1.entity.player.PlayerTickEvents;
 import fuzs.puzzleslib.api.resources.v1.DynamicPackResources;
 import fuzs.puzzleslib.api.resources.v1.PackResourcesHelper;
 import net.minecraft.client.gui.screens.ChatScreen;
@@ -40,20 +31,6 @@ public class ArmorQuickSwapClient implements ClientModConstructor {
         InteractionInputEvents.USE.register(EventPhase.BEFORE, LocalArmorStandGearHandler::onUseInteraction);
         ScreenMouseEvents.beforeMouseClick(AbstractContainerScreen.class).register(InventoryArmorClickHandler::onBeforeMouseClick);
 
-        LivingDeathCallback.EVENT.register(ShatterTickHandler::onLivingDeath);
-        LivingEvents.TICK.register(ShatterTickHandler::onLivingTick);
-        RenderLivingEvents.BEFORE.register(ShatterRenderHandler::onBeforeRenderEntity);
-
-        LivingDeathCallback.EVENT.register(MobDismembermentHandler::onLivingDeath);
-
-        PlayerTickEvents.START.register(FewerPotionParticlesHandler::onStartPlayerTick);
-        PlayerTickEvents.END.register(FewerPotionParticlesHandler::onEndPlayerTick);
-
-        InteractionInputEvents.USE.register(ClickThroughHandler::onUseInteraction);
-
-        LoadCompleteCallback.EVENT.register(ToastControlHandler::onLoadComplete);
-        AddToastCallback.EVENT.register(ToastControlHandler::onAddToast);
-
         AddToastCallback.EVENT.register(ClickableAdvancementsHandler::onAddToast);
         ScreenMouseEvents.beforeMouseClick(ChatScreen.class).register(ClickableAdvancementsHandler::onBeforeMouseClick);
         ScreenMouseEvents.beforeMouseClick(AdvancementsScreen.class).register(ClickableAdvancementsHandler::onBeforeMouseClick);
@@ -63,26 +40,6 @@ public class ArmorQuickSwapClient implements ClientModConstructor {
         ScreenMouseEvents.beforeMouseScroll(AdvancementsScreen.class).register(ClickableAdvancementsHandler::onBeforeMouseScroll);
         ScreenEvents.afterInit(AdvancementsScreen.class).register(ClickableAdvancementsHandler::onAfterInit);
         ScreenMouseEvents.beforeMouseDrag(AdvancementsScreen.class).register(ClickableAdvancementsHandler::onBeforeMouseDrag);
-
-        ClientTickEvents.START.register(PortableCraftingHandler::onStartClientTick);
-
-        ClientTickEvents.START.register(BoatItemViewHandler::onStartClientTick);
-        ClientTickEvents.END.register(BoatItemViewHandler::onEndClientTick);
-
-        LivingAttackCallback.EVENT.register(ColoredFireOverlayHandler::onLivingAttack);
-        ModelEvents.MODIFY_UNBAKED_MODEL.register(ColoredFireOverlayHandler::onModifyUnbakedModel);
-        ModelEvents.AFTER_MODEL_LOADING.register(ColoredFireOverlayHandler::onAfterModelLoading);
-        RenderBlockOverlayCallback.EVENT.register(ColoredFireOverlayHandler::onRenderBlockOverlay);
-    }
-
-    @Override
-    public void onRegisterKeyMappings(KeyMappingsContext context) {
-        context.registerKeyMapping(PortableCraftingHandler.OPEN_CRAFTING_GRID_KEY_MAPPING, KeyMappingActivationHelper.KeyActivationContext.GAME);
-    }
-
-    @Override
-    public void onRegisterParticleProviders(ParticleProvidersContext context) {
-        context.registerClientParticleProvider(ClientModRegistry.BLOOD_PARTICLE_TYPE, BloodParticle.Provider::new);
     }
 
     @Override
@@ -102,10 +59,5 @@ public class ArmorQuickSwapClient implements ClientModConstructor {
                 }
             };
         }), false));
-    }
-
-    @Override
-    public ContentRegistrationFlags[] getContentRegistrationFlags() {
-        return new ContentRegistrationFlags[]{ContentRegistrationFlags.CLIENT_PARTICLE_TYPES};
     }
 }
